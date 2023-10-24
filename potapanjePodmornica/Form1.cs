8deaa01d21fbbc83aff796c07f28b6f37320e6aa
trinstone <116944957+trinstone@users.noreply.github.com>
2023-10-24 18:29:04 +0200

    Prikazivanje elemenata programa
    Dodata metoda UnosPozicija(bool vr) koja pri pokretanju programa ne prikazuje sve elemente forme, osim dugmeta start, a pri unosu brodova prikazuje sve potrebne elemente za unos.

--- potapanjePodmornica/Form1.cs
+++ potapanjePodmornica/Form1.cs
@@ -59,14 +59,66 @@ namespace potapanjePodmornica
         }
         //pictureBox1.BackgroundImage.RotateFlip(RotateFlipType.Rotate90FlipXY);
         //pictureBox1.Refresh();
+        //imena brodova: 1a,1b,1c,1d,2a,2b,2c,3a,3b,4a
+        private void UnosPozicija(bool postoji)
+        {
+            pbx1a.Enabled = postoji;
+            pbx1a.Visible = postoji;
+            pbx1b.Enabled = postoji;
+            pbx1b.Visible = postoji;
+            pbx1c.Enabled = postoji;
+            pbx1c.Visible = postoji;
+            pbx1d.Enabled = postoji;
+            pbx1d.Visible = postoji;
+            pbx2a.Enabled = postoji;
+            pbx2a.Visible = postoji;
+            pbx2b.Enabled = postoji;
+            pbx2b.Visible = postoji;
+            pbx2c.Enabled = postoji;
+            pbx2c.Visible = postoji;
+            pbx3a.Enabled = postoji;
+            pbx3a.Visible = postoji;
+            pbx3b.Enabled = postoji;
+            pbx3b.Visible = postoji;
+            pbx4a.Enabled = postoji;
+            pbx4a.Visible = postoji;
+            pbxJa.Enabled = postoji;
+            pbxJa.Visible = postoji;
+            btnHelp.Enabled = postoji;
+            btnHelp.Visible = postoji;
+            btnSpreman.Enabled = postoji;
+            btnSpreman.Visible = postoji;
+            btnRestartPozicije.Enabled = postoji;
+            btnRestartPozicije.Visible = postoji;
+            lblIgrac1.Visible = postoji;
+            pbxAvion.Enabled = false;
+            pbxAvion.Visible = false;
+            pbxProtivnik.Enabled = false;
+            pbxProtivnik.Visible = false;
+            lblIgrac2.Enabled = false;
+            lblIgrac2.Visible = false;
+            lblIspisPobednik.Enabled = false;
+            lblIspisPobednik.Visible = false;
+            btnIgrajOpet.Enabled = false;
+            btnIgrajOpet.Visible = false;
+            btnIzlaz.Enabled = false;
+            btnIzlaz.Visible = false; 
+        }
         private void btnStartProg_Click(object sender, EventArgs e)
         {
-
+            UnosPozicija(true);
+            btnStartProg.Enabled = false;
+            btnStartProg.Visible = false;
         }
 
         private void btnSpreman_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void frmPotop_Load(object sender, EventArgs e)
+        {
+            UnosPozicija(false);
         }
     }
 }
