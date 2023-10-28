2057630b5e5b285bea9d2855fc8fda43f1a6a64e
SofijaaP <104568179+SofijaaP@users.noreply.github.com>
2023-10-28 13:43:34 +0200

    Provera da li moze da se tu postavi brod

--- potapanjePodmornica/Form1.cs
+++ potapanjePodmornica/Form1.cs
@@ -13,14 +13,80 @@ namespace potapanjePodmornica
     public partial class frmPotop : Form
     {
         static int sirinaPolja;
+        int[,] tablaPrvog = new int[10, 10];
+        int[,] tablaDrugog = new int[10, 10];
         public frmPotop()
         {
             InitializeComponent();
         }
-        
+        //0 - prazno, 1 - pogodjeno prazno, 2 - brod, 3 - pogodjen brog
         //pictureBox1.BackgroundImage.RotateFlip(RotateFlipType.Rotate90FlipXY);
         //pictureBox1.Refresh();
         //imena brodova: 1a,1b,1c,1d,2a,2b,2c,3a,3b,4a
+        static bool MozeDaSePostaviBrod(int x, int y, int duzina, bool horizontalno, int[,]tabela)
+        {
+            if (horizontalno)
+            {
+                int krajX = x + duzina - 1;
+                if (krajX > 9) return false;
+                for (int i = x; i <= krajX; i++)
+                {
+                    if (tabela[y, i] != 0) return false;
+                }
+                if (y != 0)
+                {
+                    for (int i = x; i <= krajX; i++)
+                    {
+                        if (tabela[y - 1, i] != 0) return false;
+                    }
+                }
+                if (y != 9)
+                {
+                    for (int i = x; i <= krajX; i++)
+                    {
+                        if (tabela[y + 1, i] != 0) return false;
+                    }
+                }
+                if (x != 0 && tabela[y, x - 1] != 0) return false;
+                if (krajX != 9 && tabela[y, krajX + 1] != 0) return false;
+                if (x != 0 && y != 0 && tabela[y - 1, x - 1] != 0) return false;
+                if (krajX != 9 && y != 0 && tabela[y - 1, krajX + 1] != 0) return false;
+                if (x != 0 && y != 9 && tabela[y + 1, x - 1] != 0) return false;
+                if (krajX != 9 && y != 9 && tabela[y + 1, krajX + 1] != 0) return false;
+                return true;
+
+            }
+            else
+            {
+                int krajY = y + duzina - 1;
+                if (krajY > 9) return false;
+                for (int i = y; i <= krajY; i++)
+                {
+                    if (tabela[i, x] != 0) return false;
+                }
+                if (x != 0)
+                {
+                    for (int i = y; i <= krajY; i++)
+                    {
+                        if (tabela[i, x - 1] != 0) return false;
+                    }
+                }
+                if (x != 9)
+                {
+                    for (int i = y; i <= krajY; i++)
+                    {
+                        if (tabela[i, x + 1] != 0) return false;
+                    }
+                }
+                if (y != 0 && tabela[y - 1, x] != 0) return false;
+                if (krajY != 9 && tabela[krajY + 1, x] != 0) return false;
+                if (y != 0 && x != 0 && tabela[y - 1, x - 1] != 0) return false;
+                if (krajY != 9 && x != 0 && tabela[krajY + 1, x - 1] != 0) return false;
+                if (y != 0 && x != 9 && tabela[y - 1, x + 1] != 0) return false;
+                if (krajY != 9 && x != 9 && tabela[krajY + 1, x + 1] != 0) return false;
+                return true;
+            }
+        }
         private void UnosPozicija(bool postoji)
         {
             pbx1a.Enabled = postoji;
